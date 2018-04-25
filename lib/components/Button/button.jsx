1d09ff93238b04a6b5a51c9d345c74618cb7250a
ZJH9Rondo <rajzorondo@gmail.com>
2018-04-25 20:21:40 +0800

    add:webpack编译

--- lib/components/Button/button.jsx
+++ lib/components/Button/button.jsx
@@ -1,18 +1,39 @@
 import React,{ Component } from 'react';
+import classNames from 'classnames';
+require('./style/index.less');
 
-export default class Button extends React.Component {
+export default class Button extends Component {
     constructor (props) {
-        super();
+        super(props);
+        this.state={
+            type: 'normal',
+            size: 'mid',
+            shape: 'default',
+            icon: 'null'
+        };
     }
 
     componentWillMount() {
-        console.log(this.props);
+        this.setState({
+            type: this.props.type || 'normal',
+            size: this.props.size || 'mid',
+            shape: this.props.shape || 'default',
+            icon: this.props.icon || 'null'
+        });
     }
 
     render() {
+        const {type,shape,size,icon} = this.state;
+
+        const buttonClass = classNames({
+            [`${size}`]: size,
+            [`${type}`]: type,
+            [`${shape}`]: shape,
+            [`${icon}`]: icon
+        });
         return (
             <div>
-                <button value="点击">点击</button>
+                <button className={buttonClass}>点击</button>
             </div>
         )
     }
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const srcPath = path.resolve(__dirname, './lib');
+const assetsPath = path.resolve(srcPath, 'assets');
+const isDev = (process.env.NODE_ENV !== 'production');
+
+module.exports = {
+  entry: [
+    './lib/index'
+  ],
+  output: {
+    path: path.join(__dirname, '/build'),
+    publicPath: '/',
+    libraryTarget: "umd",
+    filename: `[name]${isDev ? '' : '_[chunkhash]'}.js`,
+    chunkFilename: `[name]${isDev ? '' : '_[chunkhash]'}.js`,
+  },
+  module: {
+    rules: [
+      {
+          test: /\.(jsx|js)?$/,
+          exclude: /node_modules/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: ['es2015', 'react']
+              }
+          }
+      },
+      {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: [
+                  {
+                      loader: 'css-loader'
+                  },
+                  {
+                      loader: 'postcss-loader',
+                      options: {
+                          plugins: [autoprefixer({browsers: ['> 1%', 'last 2 versions', 'ie >= 8', 'iOS 7']})],
+                      }
+                  }
+              ]
+          })
+      },
+      {
+          test: /\.less$/,
+          use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: [
+                  {
+                      loader: 'css-loader'
+                  },
+                  {
+                      loader: 'postcss-loader',
+                      options: {
+                          plugins: [autoprefixer({browsers: ['> 1%', 'last 2 versions', 'ie >= 8', 'iOS 7']})]
+                      }
+                  },
+                  'less-loader'
+              ]
+          })
+      },
+      {
+          test: /\.(jpeg|jpg|png|gif|svg)$/,
+          use: [{
+              loader: 'url-loader',
+              options: {
+                  limit: 2048,
+                  name: `image/[name]${isDev ? '' : '_[hash]'}.[ext]`
+              }
+          }]
+      },
+      {
+          test: /\.(woff|woff2|ttf|eot)$/,
+          use: [{
+              loader: 'file-loader',
+              options: {
+                  name: `font/[name]${isDev ? '' : '_[hash]'}.[ext]`
+              }
+          }]
+      },
+      {
+          test: /\.json$/,
+          use: 'json-loader'
+      },
+  ],
+  },
+  resolve: {
+    extensions: ['.js','jsx','.less']
+  },
+  plugins: [
+    new ExtractTextPlugin(`css/index.css`, {allChunks: true})
+  ],
+  devtool: "source-map"
+};
