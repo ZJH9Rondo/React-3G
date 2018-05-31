1935049d90aabd4c06be4f7371b4ce8fa2976c18
ZJH9Rondo <779247142@qq.com>
2018-05-31 16:10:42 +0800

    add: 完成基本checkbox组件

--- lib/components/Checkbox/index.jsx
+++ lib/components/Checkbox/index.jsx
@@ -8,12 +8,21 @@ export default class Checkbox extends Component{
     }
 
     render() {
+        const { disabled, text, onChange } = this.props;
+ 
+        const disCheckbox = disabled ? 'disabled' : null;
+        const checkboxText = text ? <span className="mb-checkbox-text">{text}</span> : null;
+        /**
+         * 处理 checkbox 的 event事件
+         */
+        const checkboxChange = onChange ? onChange : null;
+
         return (
             <label className="mb-checkbox-ctn">
                 <span className="mb-checkbox-box">
-                    <input className="mb-checkbox" type="checkbox"></input>
+                    <input className="mb-checkbox" type="checkbox" onChange={checkboxChange} disabled={disCheckbox}></input>
                 </span>
-                <span className="mb-checkbox-text">复选框</span>
+                {checkboxText}
             </label>
         )
     }
